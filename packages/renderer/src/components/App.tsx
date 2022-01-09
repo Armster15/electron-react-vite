31db62bb04f41f9747642199a4dfaf5faa8a11d9
Armaan Aggarwal <armaanaggarwal@gmail.com>
2022-01-09 12:09:01 -0800

    add tailwind

---
+++ packages/renderer/src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const App: React.FC = ({}) => {
+  return (
+    <div className="flex min-h-screen">
+      <div className="flex flex-1 justify-center items-center text-center">
+        <div>
+          <h1 className="font-bold text-2xl">Hello World!</h1>
+          <p>You are running Node v{window.versions.node} amd Chrome {window.versions.chrome}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
